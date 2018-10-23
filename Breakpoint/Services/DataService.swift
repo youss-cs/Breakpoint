e9155b63d485b39e75a469a324e415a441b54181
YouSS <youss.cs@gmail.com>
2018-10-23 21:49:10 +0100

    Login & sign in views done

---
+++ Breakpoint/Services/DataService.swift
@@ -0,0 +1,41 @@
+//
+//  DataService.swift
+//  Breakpoint
+//
+//  Created by YouSS on 10/23/18.
+//  Copyright © 2018 YouSS. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+let _DB_BASE = Database.database().reference()
+
+class DataService {
+    static let instance = DataService()
+    
+    private var _REF_BASE = _DB_BASE
+    private var _DB_USERS = _DB_BASE.child("users")
+    private var _DB_GROUPS = _DB_BASE.child("groups")
+    private var _DB_FEED = _DB_BASE.child("feed")
+    
+    var REF_BASE: DatabaseReference {
+        return _REF_BASE
+    }
+    
+    var REF_USERS: DatabaseReference {
+        return _DB_USERS
+    }
+    
+    var REF_GROUPS: DatabaseReference {
+        return _DB_GROUPS
+    }
+    
+    var REF_FEED: DatabaseReference {
+        return _DB_FEED
+    }
+    
+    func createDBUser(uid: String, userData: Dictionary<String,Any>) {
+        REF_USERS.child(uid).updateChildValues(userData)
+    }
+}
